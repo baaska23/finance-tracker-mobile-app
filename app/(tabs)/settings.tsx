02b1b2fc94961ae0adf5c17512c7339622f09889
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2026-02-19 19:10:42 +0800

    update

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,24 +1,12 @@
 import FHeader from "@/components/layout/header";
-import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { Collapsible } from "@/components/ui/collapsible";
-import { IconSymbol } from "@/components/ui/icon-symbol";
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export default function SettingsScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }
-    >
+    <View style={styles.container}>
       <ThemedView style={styles.titleContainer}>
         <FHeader header="Settings" />
       </ThemedView>
@@ -30,11 +18,14 @@ export default function SettingsScreen() {
           provide files for different screen densities
         </ThemedText>
       </Collapsible>
-    </ParallaxScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   headerImage: {
     color: "#808080",
     bottom: -90,
